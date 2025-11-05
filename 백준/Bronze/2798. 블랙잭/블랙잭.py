d5e5f9ac49747aebb899fe1d256cd56002a6e9ed
JaeYeon <reenact115@gmail.com>
2025-11-05 16:24:05 +0900

    [Bronze II] Title: 블랙잭, Time: 80 ms, Memory: 38784 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,13 +1,11 @@
 N, M = map(int, input().split())
 num = list(map(int,input().split()))
-sum =[]
+sum_card =[]
 
 for i in range(N):
     for j in range(i+1, N):
         for k in range(j+1, N):
             card =  num[i] + num[j] + num[k]
-            if card > M:
-                continue
-            else:
-                sum.append(card)
-print(max(sum))
+            if card <= M:
+                sum_card.append(card)
+print(max(sum_card))
